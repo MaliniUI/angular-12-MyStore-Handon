8757d4bfb8a8f76ee43d31850c53dce1c6216e4f
MaliniUI <malinibe2010@gmail.com>
2021-07-30 22:16:13 +0530

    3 files modified, 6 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,11 +13,12 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('Notify me clicked!');
+  }
+
+  onSubscribing() {
+    window.alert('Subscribed!');
+  }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
