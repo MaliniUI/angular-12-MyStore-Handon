7a0ba66ed87862331d9ca51a834fe3ec35834615
MaliniUI <malinibe2010@gmail.com>
2021-08-02 12:07:39 +0530

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Product, products } from './products';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
+  items: Product[] = [];
+  constructor() {}
 
-  constructor() { }
+  addToCart(product: Product) {
+    return this.items.push(product);
+  }
 
+  getItemsOfCart() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
